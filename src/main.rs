b32624296707550261c81b3c0a10331a1f9475fb
AMchuAMchuAMchu <2466130840@qq.com>
2023-03-06 20:51:03 +0800

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+/**
+ * Description ==> TODO
+ * BelongsProject ==> rust_basic1001
+ * BelongsPackage ==> 
+ * Version ==> 1.0
+ * CreateTime ==> 2023-03-06 20:34:00
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+
+fn main(){
+    println!("hehe!!rust vs rust")
 }
